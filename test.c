e6387b24079ea36cb1a1f8e37cac2edf7fceb2a5
Soengyong Kim <soekim@c6r5s4.42seoul.kr>
2021-06-18 23:19:26 +0900

    need to do pipe function test

--- test.c
+++ test.c
@@ -31,14 +31,16 @@ int		main(int argc, char **argv)//, char **envp)
 //	//result : seems that when forked again in child process, 
 //	//the pid of child process get higher than 0
 	
-
-	//test for research how to pass arg to cmd
-	//result : should parse to be delimited by space
-	char **cmd_argv;
-
-	cmd_argv = (char **)malloc(3 * sizeof(char *));
-	cmd_argv[0] = strdup("ls");
-	cmd_argv[1] = ""; //for blank string, just pass it
-	cmd_argv[2] = NULL;
-	execve("/bin/ls", cmd_argv, NULL);
+//
+//	//test for research how to pass arg to cmd
+//	//result : should parse to be delimited by space
+//	char **cmd_argv;
+//
+//	cmd_argv = (char **)malloc(3 * sizeof(char *));
+//	cmd_argv[0] = strdup("ls");
+//	cmd_argv[1] = ""; //for blank string, just pass it
+//	cmd_argv[2] = NULL;
+//	execve("/bin/ls", cmd_argv, NULL);
+//
+//	need to do pipe test, if child exited fd in pipe function remains
 }
